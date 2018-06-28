3f28bec79d9c3dbeb211c85ac244d272faa9e1f7
leizhang1984 <kinkikid2005@hotmail.com>
2018-06-28 11:52:45 +0800

    2018-06-28

---
+++ ASM/StartStopASMVM/YumDMBWindowsService/AzureVMOperation.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace YumDMBWindowsService
+{
+    public class AzureVMOperation
+    {
+        public AzureVMOperation()
+        {
+
+        }
+        static X509Certificate2 Certificate;
+        public static bool CheckCertificate(string certString)
+        {
+            string certPassword = "";
+            try
+            {
+                Certificate = new X509Certificate2(Convert.FromBase64String(certString), certPassword);
+            }
+            catch (Exception ex)
+            {
+                Log.WriteLog("请检查App.config文件的thumbprint参数," + ex.Message);
+            }
+            return true;
+        }
+
+        public static void StartVM(string subscriptionID, string cloudServiceName, string deploymentsName, string vmName, int threadSleepSecond)
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(new Uri("https://management.core.chinacloudapi.cn/" + subscriptionID
+            + "/services/hostedservices/" + cloudServiceName + "/deployments/" + deploymentsName + "/roleinstances/" + vmName + "/Operations"));
+
+            request.Method = "POST";
+            request.ClientCertificates.Add(Certificate);
+            request.ContentType = "application/xml";
+            request.Headers.Add("x-ms-version", "2014-04-01");
+
+            // Add body to the request
+            XmlDocument xmlDoc = new XmlDocument();
+            //xmlDoc.Load("..\\..\\StartVM.xml");
+
+            xmlDoc.Load(ConfigurationManager.AppSettings["startvmxmlpath"].ToString());
+
+            Stream requestStream = request.GetRequestStream();
+            StreamWriter streamWriter = new StreamWriter(requestStream, System.Text.UTF8Encoding.UTF8);
+            xmlDoc.Save(streamWriter);
+
+            streamWriter.Close();
+            requestStream.Close();
+
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    response.Close();
+                    Thread.Sleep(threadSleepSecond);
+                }
+            }
+            catch (WebException ex)
+            {
+                Log.WriteLog(ex.Message);
+            }
+        }
+
+        public static void StopVM(string subscriptionID, string serviceName, string deploymentsName, string vmName, bool Deallocated, int threadSleepSecond)
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(new Uri("https://management.core.chinacloudapi.cn/" + subscriptionID
+                + "/services/hostedservices/" + serviceName + "/deployments/" + deploymentsName + "/roleinstances/" + vmName + "/Operations"));
+
+            request.Method = "POST";
+            request.ClientCertificates.Add(Certificate);
+            request.ContentType = "application/xml";
+            request.Headers.Add("x-ms-version", "2014-04-01");
+
+            //Add body to the reqeust 
+            XmlDocument xmlDoc = new XmlDocument();
+            if (Deallocated)
+            {
+                xmlDoc.Load(ConfigurationManager.AppSettings["stopvmdeallocatedxmlpath"].ToString());
+            }
+            else
+            {
+                //xmlDoc.Load("..\\..\\StopVM.xml");
+                xmlDoc.Load(ConfigurationManager.AppSettings["stopvmxmlpath"].ToString());
+            }
+
+            Stream requestStream = request.GetRequestStream();
+            StreamWriter streamWriter = new StreamWriter(requestStream, System.Text.UTF8Encoding.UTF8);
+            xmlDoc.Save(streamWriter);
+
+            streamWriter.Close();
+            requestStream.Close();
+
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    response.Close();
+                    Thread.Sleep(threadSleepSecond);
+                }
+            }
+            catch (WebException ex)
+            {
+                //Log.WriteLog(ex.Message);
+            }
+
+        }
+
+    }
+}
---
+++ ASM/StartStopASMVM/YumDMBWindowsService/Log.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YumDMBWindowsService
+{
+    public class Log
+    {
+        public static void WriteLog(string msg)
+        {
+            string logPath = ConfigurationManager.AppSettings["logpath"].Trim().ToString();
+
+            //判断是否有该文件夹           
+            if (!Directory.Exists(logPath))
+            {
+                Directory.CreateDirectory(logPath);
+            }
+
+            string logFileName = logPath + "\\" + DateTime.Now.ToString("yyyy-MM-dd") + ".log";//生成日志文件  
+            if (!File.Exists(logFileName))//判断日志文件是否为当天  
+            {
+                File.Create(logFileName);//创建文件  
+            }
+
+
+            try
+            {
+                using (FileStream fs = new System.IO.FileStream(logFileName, FileMode.Append, FileAccess.Write))
+                {
+                    using (StreamWriter writer = new System.IO.StreamWriter(fs, System.Text.Encoding.Default))
+                    {
+                        try
+                        {
+                            writer.WriteLine(DateTime.Now.ToString("HH:mm:ss") + " " + msg);
+                            writer.Flush();
+                            writer.Close();
+                            fs.Close();
+                        }
+                        catch (Exception ex)
+                        {
+                            writer.WriteLine(DateTime.Now.ToString("日志记录错误HH:mm:ss") + " " + ex.Message + " " + msg);
+                            writer.Flush();
+                            writer.Close();
+                            fs.Close();
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            { 
+
+            }
+        }
+    }
+}
