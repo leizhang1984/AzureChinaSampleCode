e43712eaa91b87b47afaa47599aa7db5cd8e4053
Lei Zhang <kinkikid2005@hotmail.com>
2021-09-26 13:37:18 +0800

    2021-09-26

---
+++ EventHub/DotNetCore/EventHubSender/EventHubSender/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using Azure.Identity;
+
+
+namespace EventHubSender
+{
+    class Program
+    {
+        // number of events to be sent to the event hub
+        private const int numOfEvents = 100;
+
+        // The Event Hubs client types are safe to cache and use as a singleton for the lifetime
+        // of the application, which is best practice when events are being published or read regularly.
+        static EventHubProducerClient producerClient;
+        
+        static async Task Main()
+        {
+            await ClientCredentialsScenarioAsync();
+
+
+            /*
+            // Create a producer client that you can use to send events to an event hub
+            producerClient = new EventHubProducerClient(connectionString, eventHubName);
+
+            // Create a batch of events 
+            EventDataBatch eventBatch;
+
+            for (int i = 1; i <= numOfEvents; i++)
+            {
+                eventBatch = await producerClient.CreateBatchAsync();
+                eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes($"Event {i}")));
+
+                await producerClient.SendAsync(eventBatch);
+                Console.WriteLine($"A Message {i} has been sent");
+
+            }
+
+            */
+        }
+
+        //输入TenantId
+        static readonly string TenantId = "";
+        //输入ClientID
+        static readonly string ClientId = "";
+        //输入App Key
+        static readonly string ClientSecret = "";
+        //Event Hub Namespace
+        static readonly string EventHubNamespace = "leieventhub01.servicebus.chinacloudapi.cn";
+        //Event Hub Name
+        static readonly string EventHubName = "event01";
+        static async Task ClientCredentialsScenarioAsync()
+        {
+            var options = new ClientCertificateCredentialOptions { AuthorityHost = AzureAuthorityHosts.AzureChina };
+            TokenCredential credential = new ClientSecretCredential(TenantId, ClientId, ClientSecret, options);
+
+
+            // Create a producer client that you can use to send events to an event hub
+            producerClient = new EventHubProducerClient(EventHubNamespace, EventHubName, credential);
+
+            // Create a batch of events 
+            EventDataBatch eventBatch;
+
+            for (int i = 1; i <= numOfEvents; i++)
+            {
+                eventBatch = await producerClient.CreateBatchAsync();
+                eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes($"Event {i}")));
+
+                await producerClient.SendAsync(eventBatch);
+                Console.WriteLine($"A Message {i} has been sent");
+
+            }
+        }
+
+    }
+}
---
+++ EventHub/DotNetCore/EventHubsReceiver/EventHubsReceiver/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Consumer;
+using Azure.Messaging.EventHubs.Processor;
+
+
+
+namespace EventHubsReceiver
+{
+    class Program
+    {
+        //Event Hub连接字符串
+        private const string ehubNamespaceConnectionString = "";
+        //Event Hub Name
+        private const string eventHubName = "event01";
+        //Blob连接字符串
+        private const string blobStorageConnectionString = "";
+        //Container name
+        private const string blobContainerName = "ehcontainer";
+
+        static BlobContainerClient storageClient;
+        // The Event Hubs client types are safe to cache and use as a singleton for the lifetime
+        // of the application, which is best practice when events are being published or read regularly.        
+        static EventProcessorClient processor;
+        static async Task Main()
+        {
+            // Read from the default consumer group: $Default
+            string consumerGroup = EventHubConsumerClient.DefaultConsumerGroupName;
+
+            // Create a blob container client that the event processor will use 
+            storageClient = new BlobContainerClient(blobStorageConnectionString, blobContainerName);
+
+            // Create an event processor client to process events in the event hub
+            processor = new EventProcessorClient(storageClient, consumerGroup, ehubNamespaceConnectionString, eventHubName);
+
+            // Register handlers for processing events and handling errors
+            processor.ProcessEventAsync += ProcessEventHandler;
+            processor.ProcessErrorAsync += ProcessErrorHandler;
+
+            // Start the processing
+            await processor.StartProcessingAsync();
+
+            // Wait for 30 seconds for the events to be processed
+            await Task.Delay(TimeSpan.FromSeconds(3000));
+
+            // Stop the processing
+            await processor.StopProcessingAsync();
+        }
+
+
+        static async Task ProcessEventHandler(ProcessEventArgs eventArgs)
+        {
+            // Write the body of the event to the console window
+            Console.WriteLine("\tReceived event: {0}", Encoding.UTF8.GetString(eventArgs.Data.Body.ToArray()));
+
+            // Update checkpoint in the blob storage so that the app receives only new events the next time it's run
+            await eventArgs.UpdateCheckpointAsync(eventArgs.CancellationToken);
+        }
+
+        static Task ProcessErrorHandler(ProcessErrorEventArgs eventArgs)
+        {
+            // Write details about the error to the console window
+            Console.WriteLine($"\tPartition '{ eventArgs.PartitionId}': an unhandled exception was encountered. This was not expected to happen.");
+            Console.WriteLine(eventArgs.Exception.Message);
+            return Task.CompletedTask;
+        }
+    }
+}
